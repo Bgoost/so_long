aed023951f825f54a54cc8c6b8e80d69404e9675
Cristina <crmanzan@gmail.com>
2024-02-23 19:53:31 +0100

    bien hecho

--- maps.c
+++ maps.c
@@ -1,12 +1,36 @@
 #include "so_long.h"
 
+void    load_xpm_img(t_imagen *img_data, t_data data, char *filename);
+unsigned int    *get_img_pixel_ptr(t_imagen *img, int x, int y);
+
 void map_generator(t_data data)
 {
     char	*relative_path = "./images.xpm";
- 	int		img_width;
- 	int		img_height;
+ 	// int		img_width;
+ 	// int		img_height;
 
 
-    data.img = mlx_xpm_file_to_image(data.mlx, relative_path, &img_width, &img_height);
-	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
+    load_xpm_img(&data.main_img, data, relative_path);
+    *get_img_pixel_ptr(&data.main_img, 15, 20) = 0x00FF00FF;
+    // data.main_img.img = mlx_xpm_file_to_image(data.mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(data.mlx, data.win, data.main_img.img, 0, 0);
+
+}
+
+void    load_xpm_img(t_imagen *img_data, t_data data, char *filename)
+{
+    img_data->img = mlx_xpm_file_to_image(data.mlx, filename, &img_data->img_width, &img_data->img_height);
+    if (!img_data->img)
+        exit(1);
+    img_data->addr = mlx_get_data_addr(img_data->img, &img_data->bits_per_pixel, &img_data->line_length, &img_data->endian);
+    if (!img_data->addr)
+        exit(1);
+}
+
+unsigned int    *get_img_pixel_ptr(t_imagen *img, int x, int y)
+{
+    char    *dst;
+
+    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+    return ((unsigned int *)dst);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: crmanzan <crmanzan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 18:28:29 by crmanzan          #+#    #+#             */
-/*   Updated: 2024/02/23 18:46:51 by crmanzan         ###   ########.fr       */
+/*   Updated: 2024/02/23 19:39:42 by crmanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,28 @@
 # define S 1
 # define D 2
 
+typedef struct s_imagen
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		img_width;
+	int		img_height;
+}				t_imagen;
+
+
 typedef struct s_sprites
 {
-	void	*player_up;
-	void	*player_down;
-	void	*player_left;
-	void	*player_right;
-	void	*background;
-	void	*strawberries;
+	t_imagen	player_up;
+	t_imagen	player_down;
+	t_imagen	player_left;
+	t_imagen	player_right;
+	t_imagen	background;
+	t_imagen	strawberries;
 }				t_sprites;
+
 
 typedef struct s_map
 {
@@ -40,15 +53,16 @@ typedef struct s_map
 	int		y;
 }				t_map;
 
+
 typedef struct s_data
 {
 	void		*mlx;
 	void		*win;
-	void		*img;
+	t_imagen	main_img;
 	int			movements;
 	int			collected;
-	t_map		*map;
-	t_sprites	*sprites;
+	t_map		map;
+	t_sprites	sprites;
 }				t_data;
 
 int  key_pressed(int keycode, void *data);
